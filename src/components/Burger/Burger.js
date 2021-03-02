c102e616e7bd9106c437909de8995e93f1e57002
Laura Yang <laurayang321@gmail.com>
2021-03-01 20:51:59 -0800

    129. Outputting Burger Ingredients Dynamically and Calculating Ingredient Sum Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,28 @@ import classes from './Burger.css';
 import BurgerIngredient from '../BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    // Object is from JavaScript, not from React
+    // keys extract the keys from a given object and turns that into an array,
+    // so it gives you an array of the keys, the values are not part of the array
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => { // get the keys as array
+                return <BurgerIngredient key={igKey + i} type={igKey} /> // get the JSX by using keys as key
+            } );
+        } )
+        .reduce((arr, el) => {
+            return arr.concat(el) // returns the array of JSX elements
+        }, []); // reduce is an array built-in function to transform array into something else
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,26 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...};
+    // }
+
+    // it is an object, it is not an array
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
     render () {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>        
         );

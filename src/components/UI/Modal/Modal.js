3a458be12b63aa50bfac565df255c0d0f68d7fe8
Laura Yang <laurayang321@gmail.com>
2021-03-11 02:15:49 -0800

    156. Changing the Folder Structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
 // functional component
 import React, { Component } from 'react';
 import classes from './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
@@ -9,7 +9,7 @@ class Modal extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         return nextProps.show !== this.props.show;
     }
-    
+
     componentDidUpdate () {
         console.log('[Modal.js] componentDidUpdate');
     }

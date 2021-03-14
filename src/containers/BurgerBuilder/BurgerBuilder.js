c87ef292747d890211fb4f9a63975f91ed879e28
Laura Yang <laurayang321@gmail.com>
2021-03-13 21:57:25 -0800

    181. Sending a POST Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from "../../axios-orders";
 
 // JavaScript object, global var needs to be capital characters
 const INGREDIENT_PRICES = {
@@ -66,7 +67,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState( { 
-            totalPrice: newPrice, 
+            totalPrice: newPrice,
             ingredients: updatedIngredients 
         } );
         this.updatePurchaseState(updatedIngredients);
@@ -83,7 +84,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            // on production, better to calculate the totalPrice to avoid user manipulate before sending it
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Max Schwarzmuller',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '41351',
+                    country: 'Germany'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     removeIngredientHandler = (type) => {
